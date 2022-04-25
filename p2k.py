e8f9066ff5229b9a0125f5e5a0e625c550229807
Maxbrand99 <maxbrand99@gmail.com>
2022-04-25 15:48:15 -0400

    Update p2k.py
    Now supports using charmz
    Added a config option for:
        Gas Price
        Using Fuel Rods
        Charm to use

--- p2k.py
+++ p2k.py
@@ -1,40 +1,95 @@
-import traceback
+# AUTHOR: https://twitter.com/maxbrand99
+
+import math
 from web3 import Web3
-import json
 import concurrent.futures
 
-# If you enjoy using this script, bananas are much appreciated: 0xd32f25Dfa932b8064A81B8254E7997CAeBc85F97
+# DONATIONS: 0xd32f25Dfa932b8064A81B8254E7997CAeBc85F97
+# I release this script for free and rely on the donations of my fellow Kongz.
 address = Web3.toChecksumAddress("")
 key = ""
-NUM_RUNS = 100
-TEAM_NUMBER = 1
+NUM_RUNS = 25
+TEAM_NUMBER = 2
+
+# Change the gas price if you want. I am not responsible for stuck txs if you decide to be cheap on gas.
+GAS_PRICE = 150
+
+# Set this to true to use fuel rods
+USE_FUEL_RODS = False
+
+# 0 = No Charm
+# 1 = Cyber Fragment
+# 2 = Rainbow Crystal
+# 3 = Promethean Relic
+CHARM_TO_USE = 1
+
 
 # DO NOT TOUCH ANYTHING BELOW THIS LINE
 
 txs = []
 out = []
 
+
 w3 = Web3(Web3.HTTPProvider('https://polygon-rpc.com/'))
-abi = '[{"inputs":[{"internalType":"uint256","name":"_teamId","type":"uint256"},{"internalType":"bool","name":"_energy","type":"bool"}],"name":"runAdventureVRF","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"}]'
-contractAddress = Web3.toChecksumAddress("0x70C575588B98C1F46B1382c706AdAf398A874e3E")
-contract = w3.eth.contract(address=Web3.toChecksumAddress(contractAddress), abi=abi)
+gameABI = '[{"inputs":[{"internalType":"uint256","name":"_teamId","type":"uint256"},{"internalType":"bool","name":"_energy","type":"bool"}],"name":"runAdventureVRF","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_teamId","type":"uint256"},{"internalType":"uint256","name":"_charm","type":"uint256"}],"name":"activateCharm","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"charmInUse","outputs":[{"internalType":"uint256","name":"charmType","type":"uint256"},{"internalType":"uint256","name":"hp","type":"uint256"}],"stateMutability":"view","type":"function"}]'
+gameAddress = Web3.toChecksumAddress("0x70C575588B98C1F46B1382c706AdAf398A874e3E")
+gameContract = w3.eth.contract(address=gameAddress, abi=gameABI)
+charmAddress = Web3.toChecksumAddress("0x7cBCCC4a1576d7A05eB6f6286206596BCBee14aC")
+charmABI = '[{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
+charmContract = w3.eth.contract(address=charmAddress, abi=charmABI)
 
-
-nonce = w3.eth.get_transaction_count(address)
-for i in range(NUM_RUNS):
-    send_txn = contract.functions.runAdventureVRF(
-        TEAM_NUMBER,
-        False
-        ).buildTransaction({
+if CHARM_TO_USE != 0:
+    if not CHARM_TO_USE in [1, 2, 3]:
+        print("INVALID CHARM NUMBER. PLEASE READ THE COMMENTS ON LINES 12-15!")
+        raise SystemExit
+    charmBalance = charmContract.functions.balanceOf(address, CHARM_TO_USE).call()
+    if charmBalance < math.ceil(NUM_RUNS / 50):
+        print("NOT ENOUGH CHARMS IN INVENTORY FOR THIS NUMBER OF RUNS. PLEASE GET MORE CHARMS OR REDUCE THE NUMBER OF RUNS.")
+        raise SystemExit
+    charmInUse = gameContract.functions.charmInUse(address, TEAM_NUMBER).call()
+    if not charmInUse[1] in [0, 1]:
+        print("THIS TEAM CURRENTLY HAS A CHARM THAT IS ALREADY IN USE. PLEASE SET CHARM NUMBER TO 0 AND RUN " + str(int(charmInUse[1] / 200)) + " MORE RUNS WITH BANANAS OR " + str(int(charmInUse[1] / (10000/30))) + " MORE RUNS WITH FUEL RODS.")
+        raise SystemExit
+    nonce = w3.eth.get_transaction_count(address)
+    completedRuns = 0
+    for i in range(math.ceil(NUM_RUNS / 50)):
+        send_txn = gameContract.functions.activateCharm(TEAM_NUMBER, CHARM_TO_USE).buildTransaction({
             'value': 0,
             'chainId': 137,
-            'gas': 500000,
-            'gasPrice': Web3.toWei(150, 'gwei'),
+            'gas': 491337,
+            'gasPrice': Web3.toWei(GAS_PRICE, 'gwei'),
             'nonce': nonce
         })
-    signed_txn = w3.eth.account.sign_transaction(send_txn, private_key=key)
-    txs.append(signed_txn)
-    nonce += 1
+        signed_txn = w3.eth.account.sign_transaction(send_txn, private_key=key)
+        txs.append(signed_txn)
+        nonce += 1
+        for j in range(50):
+            if completedRuns == NUM_RUNS:
+                break
+            completedRuns += 1
+            send_txn = gameContract.functions.runAdventureVRF(TEAM_NUMBER, USE_FUEL_RODS).buildTransaction({
+                'value': 0,
+                'chainId': 137,
+                'gas': 491337,
+                'gasPrice': Web3.toWei(GAS_PRICE, 'gwei'),
+                'nonce': nonce
+            })
+            signed_txn = w3.eth.account.sign_transaction(send_txn, private_key=key)
+            txs.append(signed_txn)
+            nonce += 1
+else:
+    nonce = w3.eth.get_transaction_count(address)
+    for i in range(NUM_RUNS):
+        send_txn = gameContract.functions.runAdventureVRF(TEAM_NUMBER, USE_FUEL_RODS).buildTransaction({
+            'value': 0,
+            'chainId': 137,
+            'gas': 491337,
+            'gasPrice': Web3.toWei(GAS_PRICE, 'gwei'),
+            'nonce': nonce
+        })
+        signed_txn = w3.eth.account.sign_transaction(send_txn, private_key=key)
+        txs.append(signed_txn)
+        nonce += 1
 
 def load_url(signed_txn):
     attempts = 0
